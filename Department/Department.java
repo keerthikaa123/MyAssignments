a5a792a5efaabab6edde8dfe670be762e109fef4
keerthikaa123 <139783230+keerthikaa123@users.noreply.github.com>
2023-08-09 20:07:06 -0700

    Add files via upload

---
+++ Department/Department.java
@@ -0,0 +1,17 @@
+package org.Department;
+
+import org.college.college;
+
+public class Department {
+	public  class department extends college
+	{
+		public void depName() {
+			System.out.println(" deptName   CS ");
+		}	
+
+		}
+		
+}
+	
+
+
---
+++ college/college.java
@@ -0,0 +1,13 @@
+package org.college;
+
+public class college {
+     public void  collegeName() {
+    	 System.out.println("Bharathithasan University");
+     }
+     public void collegeCode() {
+    	 System.out.println("PS103");
+     }
+     public void collegeRank() {
+    	 
+     }
+}
